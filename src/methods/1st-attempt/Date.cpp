de60215d393c6f16df7a96e9476cb75f03c4867f
Brian Vestergaard Danielsen <0x627664@gigaslack.com>
2024-11-13 06:36:43 +0100

    Add implementation for first attempt

---
+++ src/methods/1st-attempt/Date.cpp
@@ -0,0 +1,11 @@
+#include "../../Date.h"
+
+Date::Date(int year, int month, int day) : year(year), month(month), day(day) { }
+
+void Date::toNextDate() {
+
+}
+
+std::string Date::toString() {
+  return "";
+}
