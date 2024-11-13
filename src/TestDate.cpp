23d475e66d8cdb3b43f323cc0169be86c18d4566
Brian Vestergaard Danielsen <0x627664@gigaslack.com>
2024-11-13 07:06:45 +0100

    Add test class

---
+++ src/TestDate.cpp
@@ -0,0 +1,9 @@
+#include "TestDate.h"
+
+void TestDate::advance(Date date, std::string expected) {
+
+}
+
+void TestDate::toString(Date date, std::string expected) {
+  
+}
---
+++ src/TestDate.h
@@ -0,0 +1,12 @@
+#ifndef TEST_DATE_H
+#define TEST_DATE_H
+
+#include <string>
+#include "Date.h"
+
+class TestDate {
+  void toString(Date date, std::string expected);
+  void advance(Date date, std::string expected);
+};
+
+#endif
